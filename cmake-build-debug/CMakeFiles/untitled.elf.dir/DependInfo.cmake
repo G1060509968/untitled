0e57f68aaf65136eed8a958c617b1417e11c347e
G1060509968 <77519660+G1060509968@users.noreply.github.com>
2021-10-06 19:30:36 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/untitled.elf.dir/DependInfo.cmake
@@ -0,0 +1,82 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/startup/startup_stm32f030x6.s" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/startup/startup_stm32f030x6.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F030x6"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F0xx_HAL_Driver/Inc"
+  "../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F0xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart_ex.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart_ex.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Src/gpio.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Src/gpio.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Src/main.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Src/main.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Src/stm32f0xx_hal_msp.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Src/stm32f0xx_hal_msp.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Src/stm32f0xx_it.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Src/stm32f0xx_it.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Src/syscalls.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Src/syscalls.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Src/system_stm32f0xx.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Src/system_stm32f0xx.c.obj"
+  "C:/Users/10605/Desktop/data/STM32/example/untitled/Src/usart.c" "C:/Users/10605/Desktop/data/STM32/example/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Src/usart.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F030x6"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F0xx_HAL_Driver/Inc"
+  "../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F0xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
